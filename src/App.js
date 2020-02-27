0d2927e75f33406231b0de9bc24b120dd75a0e46
uozgan <utkuozgan_8@hotmail.com>
2020-02-27 13:49:04 +0100

    First Navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Switch, Route, NavLink } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav>
+        <NavLink
+          to="/home"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "red"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Home
+        </NavLink>{" "}
+        <NavLink
+          to="/about"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "red"
+          }}
+        >
+          About
+        </NavLink>{" "}
+        <NavLink
+          to="/discover"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "red"
+          }}
+        >
+          Discover
+        </NavLink>
+      </nav>
+      {/* <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch> */}
     </div>
   );
 }
